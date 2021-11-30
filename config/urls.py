038cf8ccceaf4fe464082aa0ecde85593f75f5bd
Max16 <max16@naver.com>
2021-11-30 22:50:05 +0900

    Superuser

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
@@ -17,7 +13,6 @@ urlpatterns = [
     path("lists/", include("lists.urls", namespace="lists")),
     path("conversations/", include("conversations.urls", namespace="conversations")),
     path("admin/", admin.site.urls),
-    path("sentry-debug/", trigger_error),
 ]
 
 if settings.DEBUG:
