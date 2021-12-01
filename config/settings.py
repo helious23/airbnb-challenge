e2679ee7f4683880749474f41389f2a70e1e451d
Max16 <max16@naver.com>
2021-12-01 09:35:41 +0900

    config

--- config/settings.py
+++ config/settings.py
@@ -196,7 +196,6 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
-    
     # Sentry
 
     sentry_sdk.init(
