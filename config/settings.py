d769037c74c0c416934cf435027aa36838a04656
Max16 <max16@naver.com>
2021-12-01 09:19:14 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -186,17 +186,17 @@ if not DEBUG:
 
     # Django Storage
 
-    # DEFAULT_FILE_STORAGE = "config.custom_storage.UploadStorage"
-    # STATICFILES_STORAGE = "config.custom_storage.StaticStorage"
-    # AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    # AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    # AWS_STORAGE_BUCKET_NAME = "airbnb-challenge-max16"
-    # AWS_DEFAULT_ACL = "public-read"
+    DEFAULT_FILE_STORAGE = "config.custom_storage.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storage.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-challenge-max16"
+    AWS_DEFAULT_ACL = "public-read"
 
-    # AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
 
-    # STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
-
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+    
     # Sentry
 
     sentry_sdk.init(
